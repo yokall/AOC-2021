cc9bae89c6a60f6ff6dc97893d8ae10ec48b73b5
Colin Campbell <colin.campbell321123@hotmail.com>
2022-02-15 18:02:06 +0000

    Day 02 solution

---
+++ day-02/lib/DayTwo/MovementProcessor.pm
@@ -0,0 +1,54 @@
+package DayTwo::MovementProcessor;
+
+use strict;
+use warnings;
+
+sub part_one {
+	my $movements = shift;
+
+	my $horizontal_position = 0;
+	my $depth = 0;
+
+	foreach my $movement (@$movements) {
+		my ($command, $value) = split(' ', $movement);
+
+		if ($command eq 'forward') {
+			$horizontal_position += $value;
+		}
+		elsif ($command eq 'down') {
+			$depth += $value;
+		}
+		elsif ($command eq 'up') {
+			$depth -= $value;
+		}
+	}
+
+	return $horizontal_position * $depth;
+}
+
+sub part_two {
+	my $movements = shift;
+
+	my $horizontal_position = 0;
+	my $depth = 0;
+	my $aim = 0;
+
+	foreach my $movement (@$movements) {
+		my ($command, $value) = split(' ', $movement);
+
+		if ($command eq 'forward') {
+			$horizontal_position += $value;
+			$depth += $aim * $value;
+		}
+		elsif ($command eq 'down') {
+			$aim += $value;
+		}
+		elsif ($command eq 'up') {
+			$aim -= $value;
+		}
+	}
+
+	return $horizontal_position * $depth;
+}
+
+1;
